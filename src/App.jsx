aeb1399726e953de52de800fa5a725645cec7a2f
FreezeP18 <sebastia66691@gmail.com>
2024-07-16 18:23:33 -0600

    App is Created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+function App() {
+  return (
+    <>
+      <main>
+        <div id="game-container">
+        <ol id="players">
+           <Player name="Player 1" symbol="X" />
+           <Player name="Player 2" symbol="O" />
+        </ol>
+        <GameBoard/>
+        </div>
+        LOG
+      </main>
+    </>
+  );
+}
+
+export default App;
